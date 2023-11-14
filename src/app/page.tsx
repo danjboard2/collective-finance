17d14b7c6f8c03418d0a90ff8324a1a1e6b8d2a8
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-11-14 22:57:02 +0000

    amends to spinner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
    <>
     <main className="h-screen w-screen flex justify-center items-center bg-[#131313]" id="splash">
-      <Image className="spin-anim" src="/media/images/collective-finance-logo-spin.png" alt="Collective Finance" height={200} width={200}></Image>
+      <Image className="spin-anim" src="/media/images/collective-finance-logo-spin.png" alt="Collective Finance" height={160} width={160}></Image>
     </main>
    </>
   )
