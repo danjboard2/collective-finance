37821868f9d3820cccd34a98830075b1c384363e
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-11-18 19:34:45 +0000

    hero

---
+++ src/app/components/Hexagons.tsx
@@ -0,0 +1,135 @@
+import { useEffect, useState, ReactElement, useRef } from 'react';
+
+function updateAnimation(i:any, lastPosition:any, baseName:any, hexagonType:any) {
+    if (!lastPosition) {
+      lastPosition = { x: 0, y: 0 };
+    }
+    const newPoints = [...Array(3)].map(() => ({
+      x: Math.random() * 300 - 150,
+      y: Math.random() * 300 - 150
+    }));
+  
+    const keyframes = `
+    @keyframes floatAnimation${baseName}${i} {
+        0% { transform: translate(${lastPosition.x}px, ${lastPosition.y}px); }
+        33% { transform: translate(${newPoints[0].x}px, ${newPoints[0].y}px); }
+        66% { transform: translate(${newPoints[1].x}px, ${newPoints[1].y}px); }
+        100% { transform: translate(${newPoints[2].x}px, ${newPoints[2].y}px); }
+      }
+    `;
+    try {
+      document.styleSheets[0].insertRule(keyframes, document.styleSheets[0].cssRules.length);
+    } catch (e) {
+      console.error('Error inserting keyframes:', e);
+    }
+    console.log(`Animation ${baseName}${i}: Start - x: ${lastPosition.x}, y: ${lastPosition.y}`);
+    console.log(`Animation ${baseName}${i}: End - x: ${newPoints[2].x}, y: ${newPoints[2].y}`);
+    
+    // Differentiate the debug message based on hexagonType
+    console.log(`Next cycle start for ${baseName}${i}: x: ${newPoints[2].x}, y: ${newPoints[2].y} (${hexagonType} hexagon)`);
+  
+    return { x: newPoints[2].x, y: newPoints[2].y }; // Return new last position
+  }
+  function getRandomStyle(animationName:any) {
+    // Random size between 50px and 100px
+    const size = Math.random() * (150 - 50) + 50;
+  
+    // Random opacity between 0.4 and 1
+    const opacity = Math.random() * (0.6 - 0.2) + 0.2;
+  
+    // Default values for server-side rendering
+    let maxTop = 100;
+    let maxLeft = 100;
+  
+  
+    const animationDuration = `${Math.random() * 5 + 10}s`; // Random between 10 and x seconds
+    const animationDelay = `${Math.random() * 2}s`; // Random up to 2 seconds delay
+  
+    // Adjusting the position limits based on size
+    if (typeof window !== 'undefined') {
+      maxTop = 100 - (size / window.innerHeight) * 100;
+      maxLeft = 100 - (size / window.innerWidth) * 100;
+    }
+  
+    const top = Math.random() * maxTop;
+    const left = Math.random() * maxLeft;
+  
+    return { top: `${top}%`, left: `${left}%`, width: `${size}px`, height: `${size}px`, opacity, animation: `${animationName} ${animationDuration} ${animationDelay} infinite linear` };
+  }
+  function Hexagons() {
+    const [hexagons, setHexagons] = useState<ReactElement[]>([]);
+    const lastPositions = useRef([...Array(1000)].map(() => ({ x: 0, y: 0 })));
+  
+    useEffect(() => {
+      const newHexagons = [];
+      const lastPositionFilled = { x: 0, y: 0 };
+      const lastPositionHollow = { x: 0, y: 0 };
+      for (let i = 0, j = 30; i < 20; i++, j++) {
+        // For filled hexagons
+        lastPositions.current[i] = updateAnimation(i, lastPositionFilled, 'Filled', 'Filled');
+        lastPositions.current[j] = updateAnimation(j, lastPositionHollow, 'Hollow', 'Hollow');
+  
+          const animationNameFilled = `floatAnimationFilled${i}`;
+          const animationNameHollow = `floatAnimationHollow${j}`;
+  
+          const filledHexagonStyle = getRandomStyle(animationNameFilled);
+          const hollowHexagonStyle = getRandomStyle(animationNameHollow); 
+  
+          newHexagons.push(
+            <svg key={`filled-${i}`} data-index={`filled-${i}`} data-type="filled" style={{ ...filledHexagonStyle, position: 'absolute' }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 184.751 184.751" >
+            <path d="M0,92.375l46.188-80h92.378l46.185,80l-46.185,80H46.188L0,92.375z"/>
+          </svg>,
+          <svg key={`hollow-${j}`} data-index={`hollow-${j}`} data-type="hollow" style={{ ...hollowHexagonStyle, position: 'absolute' }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 485.688 485.688">
+            <path d="M364.269,453.155H121.416L0,242.844L121.416,32.533h242.853l121.419,210.312L364.269,453.155z M131.905,434.997h221.878 l110.939-192.152L353.783,50.691H131.905L20.966,242.844L131.905,434.997z" />
+          </svg>
+        );
+      }
+  
+      setHexagons(newHexagons);
+    }, []);
+  
+    useEffect(() => {
+      function onAnimationIteration(e:any) {
+        const dataIndex = e.target.dataset.index; // This will be something like 'filled-1' or 'hollow-1'
+        const [type, indexStr] = dataIndex.split('-');
+        const index = parseInt(indexStr, 10);
+        const isHollow = type === "hollow";
+        let hexagonType = isHollow ? "Hollow" : "Filled";
+        const baseName = isHollow ? "Hollow" : "Filled";
+        console.log(`Animation cycle completed for ${baseName} hexagon ${index}`);
+        //console.log(`Next cycle start for ${baseName}${index}: x: ${lastPositions.current[index].x}, y: ${lastPositions.current[index].y}`);
+      
+  
+        requestAnimationFrame(() => {
+          const newPosition = updateAnimation(index, lastPositions.current[index], baseName, hexagonType);
+          console.log(`Next cycle start for ${baseName}${index}: x: ${newPosition.x}, y: ${newPosition.y}`);
+          lastPositions.current[index] = newPosition;
+        });
+      }
+    
+     // Attach event listeners
+    hexagons.forEach(hexagon => {
+      const dataIndex = hexagon.props['data-index'];
+      const element = document.querySelector(`[data-index='${dataIndex}']`);
+      if (element) {
+        element.addEventListener('animationiteration', onAnimationIteration);
+      }
+    });
+  
+    // Detach event listeners on cleanup
+    return () => {
+      hexagons.forEach(hexagon => {
+        const dataIndex = hexagon.props['data-index'];
+        const element = document.querySelector(`[data-index='${dataIndex}']`);
+        if (element) {
+          element.removeEventListener('animationiteration', onAnimationIteration);
+        }
+      });
+    };
+  }, [hexagons]);
+    
+  
+    return <>{hexagons}</>;
+  }
+
+  export default Hexagons
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,148 +1,9 @@
 "use client";
 import Image from 'next/image';
-import { useEffect, useState, ReactElement, useRef } from 'react';
+import { useEffect, useState} from 'react';
 import { useCookies } from 'react-cookie';
+import Hexagons from '../app/components/Hexagons'
 
-function updateAnimation(i:any, lastPosition:any, baseName:any, hexagonType:any) {
-  if (!lastPosition) {
-    lastPosition = { x: 0, y: 0 };
-  }
-  const newPoints = [...Array(3)].map(() => ({
-    x: Math.random() * 300 - 150,
-    y: Math.random() * 300 - 150
-  }));
-
-  const keyframes = `
-  @keyframes floatAnimation${baseName}${i} {
-      0% { transform: translate(${lastPosition.x}px, ${lastPosition.y}px); }
-      33% { transform: translate(${newPoints[0].x}px, ${newPoints[0].y}px); }
-      66% { transform: translate(${newPoints[1].x}px, ${newPoints[1].y}px); }
-      100% { transform: translate(${newPoints[2].x}px, ${newPoints[2].y}px); }
-    }
-  `;
-  try {
-    document.styleSheets[0].insertRule(keyframes, document.styleSheets[0].cssRules.length);
-  } catch (e) {
-    console.error('Error inserting keyframes:', e);
-  }
-  console.log(`Animation ${baseName}${i}: Start - x: ${lastPosition.x}, y: ${lastPosition.y}`);
-  console.log(`Animation ${baseName}${i}: End - x: ${newPoints[2].x}, y: ${newPoints[2].y}`);
-  
-  // Differentiate the debug message based on hexagonType
-  console.log(`Next cycle start for ${baseName}${i}: x: ${newPoints[2].x}, y: ${newPoints[2].y} (${hexagonType} hexagon)`);
-
-  return { x: newPoints[2].x, y: newPoints[2].y }; // Return new last position
-}
-function getRandomStyle(animationName:any) {
-  // Random size between 50px and 100px
-  const size = Math.random() * (100 - 50) + 50;
-
-  // Random opacity between 0.4 and 1
-  const opacity = Math.random() * (0.6 - 0.2) + 0.2;
-
-  // Default values for server-side rendering
-  let maxTop = 100;
-  let maxLeft = 100;
-
-
-  const animationDuration = `${Math.random() * 5 + 10}s`; // Random between 10 and x seconds
-  const animationDelay = `${Math.random() * 2}s`; // Random up to 2 seconds delay
-
-  // Adjusting the position limits based on size
-  if (typeof window !== 'undefined') {
-    maxTop = 100 - (size / window.innerHeight) * 100;
-    maxLeft = 100 - (size / window.innerWidth) * 100;
-  }
-
-  const top = Math.random() * maxTop;
-  const left = Math.random() * maxLeft;
-
-  return { top: `${top}%`, left: `${left}%`, width: `${size}px`, height: `${size}px`, opacity, animation: `${animationName} ${animationDuration} ${animationDelay} infinite linear` };
-}
-function Hexagons() {
-  const [hexagons, setHexagons] = useState<ReactElement[]>([]);
-  const lastPositions = useRef([...Array(15)].map(() => ({ x: 0, y: 0 })));
-
-  useEffect(() => {
-    const newHexagons = [];
-    const lastPositionFilled = { x: 0, y: 0 };
-    const lastPositionHollow = { x: 0, y: 0 };
-    for (let i = 0; i < 15; i++) {
-      const j = i+15;
-      lastPositions.current[i] = updateAnimation(i, lastPositionFilled, 'Filled', 'Filled');
-      lastPositions.current[j] = updateAnimation(j, lastPositionHollow, 'Hollow', 'Hollow');
-
-        const animationNameFilled = `floatAnimationFilled${i}`;
-        const animationNameHollow = `floatAnimationHollow${j}`;
-
-        const filledHexagonStyle = getRandomStyle(animationNameFilled);
-        const hollowHexagonStyle = getRandomStyle(animationNameHollow); 
-
-        newHexagons.push(
-          <svg key={`filled-${i}`} data-index={i} data-type="filled" style={{ ...filledHexagonStyle, position: 'absolute' }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 184.751 184.751">
-          <path d="M0,92.375l46.188-80h92.378l46.185,80l-46.185,80H46.188L0,92.375z"/>
-        </svg>,
-        <svg key={`hollow-${j}`} data-index={j} data-type="hollow" style={{ ...hollowHexagonStyle, position: 'absolute' }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 485.688 485.688">
-          <path d="M364.269,453.155H121.416L0,242.844L121.416,32.533h242.853l121.419,210.312L364.269,453.155z M131.905,434.997h221.878 l110.939-192.152L353.783,50.691H131.905L20.966,242.844L131.905,434.997z"/>
-        </svg>
-      );
-    }
-
-    setHexagons(newHexagons);
-  }, []);
-
-  useEffect(() => {
-    function onAnimationIteration(e:any) {
-      const index = parseInt(e.target.dataset.index, 10);
-      const isHollow = e.target.dataset.type === "hollow";
-      let hexagonType: string; // or any other appropriate type
-      const baseName = isHollow ? "Hollow" : "Filled";
-      console.log(`Animation cycle completed for ${baseName} hexagon ${index}`);
-      console.log(`Next cycle start for ${baseName}${index}: x: ${lastPositions.current[index].x}, y: ${lastPositions.current[index].y}`);
-    
-
-      requestAnimationFrame(() => {
-        const newPosition = updateAnimation(index, lastPositions.current[index], baseName, hexagonType);
-        console.log(`Next cycle start for ${baseName}${index}: x: ${newPosition.x}, y: ${newPosition.y}`);
-        lastPositions.current[index] = newPosition;
-      });
-    }
-  
-    // Remove the 'hexagon' parameter as it's not used
-    hexagons.forEach((_, i) => {
-      const element = document.querySelector(`[data-index='${i}']`);
-      if (element) {
-        element.addEventListener('animationiteration', onAnimationIteration);
-      }
-    });
-    hexagons.forEach((_, j) => {
-      const element = document.querySelector(`[data-index='${j}']`);
-      if (element) {
-        element.addEventListener('animationiteration', onAnimationIteration);
-      }
-      
-    });
-  
-    return () => {
-      // Similarly, remove the 'hexagon' parameter here
-      hexagons.forEach((_, i) => {
-        const element = document.querySelector(`[data-index='${i}']`);
-        if (element) {
-          element.removeEventListener('animationiteration', onAnimationIteration);
-        }
-      });
-      hexagons.forEach((_, j) => {
-        const element = document.querySelector(`[data-index='${j}']`);
-        if (element) {
-          element.removeEventListener('animationiteration', onAnimationIteration);
-        }
-      });
-    };
-  }, [hexagons]);
-  
-
-  return <>{hexagons}</>;
-}
 export default function Home() {
   const [cookies, setCookie] = useCookies(['splashShown']);
   const [isSplashShown, setIsSplashShown] = useState(false);
@@ -173,8 +34,20 @@ export default function Home() {
           <Image className="spin-anim" src="/media/images/collective-finance-logo-spin.png" alt="Collective Finance" height={160} width={160}></Image>
       </main>
         )}
-      <main className={`h-[500vh] w-full flex bg-[#131313]`} id="home">
+      <main className={`h-[100vh] w-full flex bg-[#131313]`} id="home">
         <section className="relative w-full h-screen" id="hero">
+          <div className="flex flex-row w-full h-full">
+            <div className="w-[40%] bg-black bg-opacity-50 h-full flex justify-center flex-col relative z-10 pl-28">
+                <h1 id="gold" className="text-white font-bold text-[120px] mb-0">Gold</h1>
+                <h1 id="silver"className="text-white font-bold text-[120px]  -mt-[50px] mb-0">Silver</h1>
+                <h1 id="diamond" className="text-white font-bold text-[120px] -mt-[50px] mb-0">Diamond</h1>
+                <h2 className="text-white font-light text-[45px] max-w-[550px] leading-tight mt-0">Earn passive yield on the commodities asset class</h2>
+                <button className="text-white hover:text-black hover:bg-white duration-300 flex items-center border-2 text-3xl mt-20 py-1 justify-around w-[300px] px-6"><Image className="hover:spin-anim" src="/media/images/collective-finance-logo.png" alt="Collective Finance" height={40} width={40}></Image> Open dApp</button>
+            </div>
+            <div className="w-[60%] flex-row relative z-10 overflow-hidden">
+              <Image className="max-w-none pl-40" src="/media/images/collective-finance-logo.png" alt="Collective Finance" height={1600} width={1600}></Image>
+            </div>
+          </div>
           <div id="bg-layer" className="pt-[100px] absolute top-0 bottom-0 left-0 right-0  overflow-hidden">
             <Hexagons />
           </div>
